f1254224bcfe1735e037e6d15b04d00f32343bf4
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2019-10-28 14:28:45 +0200

    Removed useless printlns

--- dependency-resolution/app/submodule-java/src/main/java/lt/rieske/samples/submodule/java/SubmoduleJava.java
+++ dependency-resolution/app/submodule-java/src/main/java/lt/rieske/samples/submodule/java/SubmoduleJava.java
@@ -7,13 +7,11 @@ import org.msgpack.core.MessagePack;
 public class SubmoduleJava {
 
     public static void useDependency() {
-        System.out.println("SubmoduleJava");
         var packer = MessagePack.newDefaultBufferPacker();
         System.out.println(packer);
     }
 
     public static MessageBufferPacker getDependency() {
-        System.out.println("SubmoduleJava");
         return MessagePack.newDefaultBufferPacker();
     }
 }
--- dependency-resolution/app/submodule-javalib/src/main/java/lt/rieske/samples/submodule/javalib/SubmoduleJavaLib.java
+++ dependency-resolution/app/submodule-javalib/src/main/java/lt/rieske/samples/submodule/javalib/SubmoduleJavaLib.java
@@ -5,13 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class SubmoduleJavaLib {
 
     public static void useDependency() {
-        System.out.println("SubmoduleJavaLib");
         var objectMapper = new ObjectMapper();
         System.out.println(objectMapper);
     }
 
     public static ObjectMapper getDependency() {
-        System.out.println("SubmoduleJavaLib");
         return new ObjectMapper();
     }
 }
